648b166225226f759cc5bf0f9b5152455e4c7bff
Max Zakharzhevskiy <max.za@digitalpersona.com>
2019-01-18 04:20:48 -0800

    Separated line logic to functions

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ function main() {
         y: number;
     }
 
-    interface ICPoint {
+    interface ILine {
         p1: IPoint;
         p2: IPoint;
         cp1?: IPoint;
@@ -15,23 +15,7 @@ function main() {
     let canvas: HTMLCanvasElement,
         c: CanvasRenderingContext2D,
         code: HTMLPreElement,
-        point: ICPoint,
-        style,
-        dpoint: IPoint;
-
-    function init(quad: boolean) {
-        point = {
-            p1: { x: 20, y: 20 }, /* starting point */
-            p2: { x: 370, y: 300 } /* end point */
-        };
-
-        if (quad) {
-            point.cp1 = { x: 370, y: 20 };
-        } else {
-            point.cp1 = { x: 70, y: 100 };
-            point.cp2 = { x: 70, y: 100 };
-        }
-
+        line: ILine,
         style = {
             curve: {
                 width: 6,
@@ -54,7 +38,26 @@ function main() {
                 arc1: 0,
                 arc2: 2 * Math.PI
             }
+        };
+
+    function initLine(quad: boolean): ILine {
+        let line: ILine = {
+            p1: { x: 20, y: 20 }, /* starting point */
+            p2: { x: 370, y: 300 } /* end point */
+        };
+
+        if (quad) {
+            line.cp1 = { x: 370, y: 20 };
+        } else {
+            line.cp1 = { x: 70, y: 100 };
+            line.cp2 = { x: 70, y: 100 };
         }
+
+        return line;
+    }
+
+    function init(quad: boolean) {
+        line = initLine(quad);
 
         // line style
         c.lineCap = 'round';
@@ -68,30 +71,20 @@ function main() {
         draw();
     }
 
-    function draw() {
-
-        c.clearRect(0, 0, canvas.width, canvas.height);
-
-        // bg gradient
-        let gradient = c.createLinearGradient(0, 0, 100, 200);
-        gradient.addColorStop(0, 'hsla(68, 46%, 50%, .2)');
-        gradient.addColorStop(1, 'hsla(58, 100%, 50%, .1)');
-        c.fillStyle = gradient;
-        c.fillRect(0, 0, 400, 500);
-
+    function drawLine(ln: ILine) {
         // lines
         c.lineWidth = style.pline.width;
         c.strokeStyle = style.pline.color;
 
         c.beginPath();
-        c.moveTo(point.p1.x, point.p1.y);
-        c.lineTo(point.cp1.x, point.cp1.y);
+        c.moveTo(ln.p1.x, ln.p1.y);
+        c.lineTo(ln.cp1.x, ln.cp1.y);
 
-        if (point.cp2) {
-            c.moveTo(point.p2.x, point.p2.y);
-            c.lineTo(point.cp2.x, point.cp2.y);
+        if (ln.cp2) {
+            c.moveTo(ln.p2.x, ln.p2.y);
+            c.lineTo(ln.cp2.x, ln.cp2.y);
         } else {
-            c.lineTo(point.p2.x, point.p2.y);
+            c.lineTo(ln.p2.x, ln.p2.y);
         }
 
         c.stroke();
@@ -101,59 +94,79 @@ function main() {
         c.strokeStyle = style.curve.color;
 
         c.beginPath();
-        c.moveTo(point.p1.x, point.p1.y);
+        c.moveTo(ln.p1.x, ln.p1.y);
 
-        if (point.cp2) {
-            c.bezierCurveTo(point.cp1.x, point.cp1.y, point.cp2.x, point.cp2.y, point.p2.x, point.p2.y);
+        if (ln.cp2) {
+            c.bezierCurveTo(ln.cp1.x, ln.cp1.y, ln.cp2.x, ln.cp2.y, ln.p2.x, ln.p2.y);
         } else {
-            c.quadraticCurveTo(point.cp1.x, point.cp1.y, point.p2.x, point.p2.y);
+            c.quadraticCurveTo(ln.cp1.x, ln.cp1.y, ln.p2.x, ln.p2.y);
         }
         c.stroke();
 
         // control points
-        for (var p in point) {
+        for (var p in ln) {
             c.lineWidth = style.circles.width;
             c.strokeStyle = style.circles.color;
             c.fillStyle = style.circles.fill;
 
             c.beginPath();
-            c.arc(point[p].x, point[p].y, style.point.radius, style.point.arc1, style.point.arc2, true);
+            c.arc(ln[p].x, ln[p].y, style.point.radius, style.point.arc1, style.point.arc2, true);
             c.fill();
             c.stroke();
         }
+    } //drawLine()
+
+    function draw() {
+        c.clearRect(0, 0, canvas.width, canvas.height);
+
+        // bg gradient
+        let gradient = c.createLinearGradient(0, 0, 100, 200);
+        gradient.addColorStop(0, 'hsla(68, 46%, 50%, .2)');
+        gradient.addColorStop(1, 'hsla(58, 100%, 50%, .1)');
+        c.fillStyle = gradient;
+        c.fillRect(0, 0, 400, 500);
+
+        drawLine(line);
 
         showCode();
     }
 
-    function showCode() {
+    function genLine(l: ILine) {
+        return "c.beginPath();\n" +
+            `c.moveTo(${l.p1.x}, ${l.p1.y});\n` +
+            (l.cp2 ?
+                `c.bezierCurveTo(${l.cp1.x}, ${l.cp1.y}, ${l.cp2.x}, ${l.cp2.y}, ${l.p2.x}, ${l.p2.y});\n` :
+                `c.quadraticCurveTo(${l.cp1.x}, ${l.cp1.y}, ${l.p2.x}, ${l.p2.y});\n`
+            ) +
+            "c.stroke();";
+    } //genLine()
 
+    function showCode() {
         if (code) {
-            code.firstChild.nodeValue =
+            let txt = 
                 "canvas = document.getElementById('canvas');\n" +
                 "c = canvas.getContext('2d');\n" +
-                `c.lineWidth = ${style.curve.width};\n` +
-                "c.beginPath();\n" +
-                `c.moveTo(${point.p1.x}, ${point.p1.y});\n` +
-                (point.cp2 ?
-                    `c.bezierCurveTo(${point.cp1.x}, ${point.cp1.y}, ${point.cp2.x}, ${point.cp2.y}, ${point.p2.x}, ${point.p2.y});` :
-                    `c.quadraticCurveTo(${point.cp1.x}, ${point.cp1.y}, ${point.p2.x}, ${point.p2.y});`
-                ) +
-                "\nc.stroke();";
+                `c.lineWidth = ${style.curve.width};\n`;
+
+            txt += genLine(line);
+
+            code.firstChild.nodeValue = txt;
         }
     } //showCode()
 
     // dragging
 
     let drag = null;
+    let dpoint: IPoint;
 
     function dragStart(event) {
         event = mousePos(event);
 
         // find nearest point
         var dx, dy;
-        for (var p in point) {
-            dx = point[p].x - event.x;
-            dy = point[p].y - event.y;
+        for (var p in line) {
+            dx = line[p].x - event.x;
+            dy = line[p].y - event.y;
 
             if ((dx * dx) + (dy * dy) < style.point.radius * style.point.radius) {
                 drag = p;
@@ -167,8 +180,8 @@ function main() {
     function dragging(event) {
         if (drag) {
             event = mousePos(event);
-            point[drag].x += event.x - dpoint.x;
-            point[drag].y += event.y - dpoint.y;
+            line[drag].x += event.x - dpoint.x;
+            line[drag].y += event.y - dpoint.y;
             dpoint = event;
 
             draw();
