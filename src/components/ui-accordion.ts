198825fc06ff9584804e8e9130a4125547143c22
Max Zakharzhevskiy <maxzz@msn.com>
2022-10-04 03:02:32 -0700

    WIP. ui-accordion.ts. update methods visibility

--- src/components/ui-accordion.ts
+++ src/components/ui-accordion.ts
@@ -18,7 +18,7 @@ export class Accordion {            //https://codepen.io/chriscoyier/pen/XWNqxyY
         this.summary.addEventListener('click', (e) => this.onClick(e)); // Detect user clicks on the summary element
     }
 
-    onClick(e: MouseEvent) {
+    private onClick(e: MouseEvent) {
         e.preventDefault();
         this.el.style.overflow = 'hidden'; // Add an overflow on the <details> to avoid content overflowing
 
@@ -27,6 +27,16 @@ export class Accordion {            //https://codepen.io/chriscoyier/pen/XWNqxyY
         } else if (this.isExpanding || this.el.open) { // Check if the element is being openned or is already open
             this.shrink();
         }
+    }
+    
+    private expand() {
+        this.isExpanding = true; // Set the element as "being expanding"
+        const startHeight = `${this.el.offsetHeight}px`; // Get the current fixed height of the element
+        const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`; // Calculate the open height of the element (summary height + content height)
+
+        this.startAmimation(startHeight, endHeight);
+        this.animation!.onfinish = () => this.onAnimationFinish(true);
+        this.animation!.oncancel = () => this.isExpanding = false;
     }
 
     private startAmimation(startHeight: string, endHeight: string) {
@@ -40,58 +50,28 @@ export class Accordion {            //https://codepen.io/chriscoyier/pen/XWNqxyY
         });
     }
 
-    shrink() {
+    private onAnimationFinish(open: boolean) {
+        this.el.open = open;
+        this.animation = null;
+        this.isClosing = false;
+        this.isExpanding = false;
+        this.el.style.height = this.el.style.overflow = ''; // Remove the overflow hidden and the fixed height
+    }
+
+    public shrink() {
         this.isClosing = true;
 
         const startHeight = `${this.el.offsetHeight}px`; // Store the current height of the element
         const endHeight = `${this.summary.offsetHeight}px`; // Calculate the height of the summary
 
         this.startAmimation(startHeight, endHeight);
-
-        // this.animation && this.animation.cancel();
-
-        // this.animation = this.el.animate({
-        //     height: [startHeight, endHeight] // Set the keyframes from the startHeight to endHeight
-        // }, {
-        //     duration: 200,
-        //     easing: 'ease-out'
-        // });
-
         this.animation!.onfinish = () => this.onAnimationFinish(false);
         this.animation!.oncancel = () => this.isClosing = false;
     }
 
-    open() {
+    public open() {
         this.el.style.height = `${this.el.offsetHeight}px`; // Apply a fixed height on the element
         this.el.open = true; // Force the [open] attribute on the details element
         window.requestAnimationFrame(() => this.expand()); // Wait for the next frame to call the expand function
-    }
-
-    expand() {
-        this.isExpanding = true; // Set the element as "being expanding"
-        const startHeight = `${this.el.offsetHeight}px`; // Get the current fixed height of the element
-        const endHeight = `${this.summary.offsetHeight + this.content.offsetHeight}px`; // Calculate the open height of the element (summary height + content height)
-
-        this.startAmimation(startHeight, endHeight);
-
-        // this.animation && this.animation.cancel();
-
-        // this.animation = this.el.animate({
-        //     height: [startHeight, endHeight] // Set the keyframes from the startHeight to endHeight
-        // }, {
-        //     duration: 200,
-        //     easing: 'ease-out'
-        // });
-
-        this.animation!.onfinish = () => this.onAnimationFinish(true);
-        this.animation!.oncancel = () => this.isExpanding = false;
-    }
-
-    onAnimationFinish(open: boolean) {
-        this.el.open = open;
-        this.animation = null;
-        this.isClosing = false;
-        this.isExpanding = false;
-        this.el.style.height = this.el.style.overflow = ''; // Remove the overflow hidden and the fixed height
     }
 }
