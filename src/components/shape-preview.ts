651f0ac4a3e285b99264159e37a970d5db9f975b
Max Zakharzhevskiy <maxzz@msn.com>
2022-10-05 18:35:43 -0700

    WIP. Previews. add singleBox action handler and curve switch

--- src/components/shape-preview.ts
+++ src/components/shape-preview.ts
@@ -1,3 +1,4 @@
+import { draw } from "./app";
 import { AppContext, ILine } from "./types";
 
 export class Previews {
@@ -9,19 +10,30 @@ export class Previews {
         this.container = document.getElementById('previews')!;
     }
 
-
-    private singleBox(line: ILine[]) {
+    private singleBox(line: ILine[], idx: number) {
         return `
-            <div class="p-4 w-12 h-12 border-slate-400 border rounded grid items-center justify-center">
+            <div class="p-4 w-12 h-12 border-slate-400 border rounded cursor-pointer grid items-center justify-center preview-box" data-idx="${idx}">
                 ${line.length}
             </div>`;
     }
 
     public update() {
-        const boxes = this.appContext.lines.map((line) => {
-            return this.singleBox(line);
+        const boxes = this.appContext.lines.map((line, idx) => {
+            return this.singleBox(line, idx);
         });
         this.container.innerHTML = boxes.join('\n');
+
+        const boxeEls = [...this.container.querySelectorAll('.preview-box')] as HTMLElement[];
+        boxeEls.forEach((box) => {
+            box.addEventListener('click', (event) => {
+                const el = event.target as HTMLElement;
+                if (el) {
+                    //console.log('el.dataset.idx', el.dataset.idx, this);
+                    this.appContext.line = this.appContext.lines[+(el.dataset.idx || 0)];
+                    draw(this.appContext);
+                }
+            })
+        })
     }
 
 }
