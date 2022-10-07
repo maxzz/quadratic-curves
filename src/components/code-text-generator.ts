0abab57dfa59ca7fe285b7f26ae41aff82f2f941
Max Zakharzhevskiy <maxzz@msn.com>
2022-10-07 01:42:58 -0700

    WIP. generateCodeText. update genLineAsArray() to use CurvePoints

--- src/components/code-text-generator.ts
+++ src/components/code-text-generator.ts
@@ -2,7 +2,6 @@ import { GRAPHSTYLE } from "./initials";
 import { ILine, linePtsToCurvePts } from "./types";
 
 function genLine(l: ILine) {
-    const pts = l.points;
     const [p1, p2, c1, c2] = linePtsToCurvePts(l.points);
     const path = c2
         ? `ctx.bezierCurveTo(${c1[0]}, ${c1[1]}, ${c2[0]}, ${c2[1]}, ${p2[0]}, ${p2[1]});`
@@ -10,26 +9,12 @@ function genLine(l: ILine) {
     return `ctx.beginPath();\nctx.moveTo(${p1[0]}, ${p1[1]});\n${path}\nctx.stroke();\n`;
 }
 
-// function genLine(l: ILine) {
-//     const pts = l.points;
-//     return "ctx.beginPath();\n" +
-//         `ctx.moveTo(${pts.p1.x}, ${pts.p1.y});\n` +
-//         (pts.cp2
-//             ? `ctx.bezierCurveTo(${pts.cp1.x}, ${pts.cp1.y}, ${pts.cp2.x}, ${pts.cp2.y}, ${pts.p2.x}, ${pts.p2.y});\n`
-//             : `ctx.quadraticCurveTo(${pts.cp1.x}, ${pts.cp1.y}, ${pts.p2.x}, ${pts.p2.y});\n`
-//         ) +
-//         "ctx.stroke();\n";
-// }
-
 function genLineAsArray(l: ILine) {
-    const pts = l.points;
-    return "{ " +
-        `p1: {x: ${pts.p1.x}, y: ${pts.p1.y}}, p2: {x: ${pts.p2.x}, y: ${pts.p2.y}}, ` +
-        (pts.cp2
-            ? `cp1: {x: ${pts.cp1.x}, y: ${pts.cp1.y}}, cp2: {x: ${pts.cp2.x}, y: ${pts.cp2.y}}`
-            : `cp1: {x: ${pts.cp1.x}, y: ${pts.cp1.y}}`
-        ) +
-        " }";
+    const [p1, p2, c1, c2] = linePtsToCurvePts(l.points);
+    const path = c2
+        ? `cp1: {x: ${c1[0]}, y: ${c1[1]}}, cp2: {x: ${c2[0]}, y: ${c2[1]}}`
+        : `cp1: {x: ${c1[0]}, y: ${c1[1]}}`;
+    return `{ p1: {x: ${p1[0]}, y: ${p1[1]}}, p2: {x: ${p2[0]}, y: ${p2[1]}}, ${path}  }`;
 }
 
 function genAll(lines: ILine[]) {
