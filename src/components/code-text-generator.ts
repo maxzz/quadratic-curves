92ad3d6b7018345ed44e914a987e60dd68d027ce
Max Zakharzhevskiy <maxzz@msn.com>
2022-10-08 16:17:18 -0700

    WIP. code generation. add numbers padding

--- src/components/code-text-generator.ts
+++ src/components/code-text-generator.ts
@@ -1,5 +1,5 @@
+import { SingleCurve, XY } from "./types";
 import { GRAPHSTYLE } from "./initials";
-import { SingleCurve } from "./types";
 
 function genLine(line: SingleCurve) {
     const [p1, p2, c1, c2] = line.points;
@@ -9,19 +9,13 @@ function genLine(line: SingleCurve) {
     return `ctx.beginPath();\nctx.moveTo(${p1[0]}, ${p1[1]});\n${path}\nctx.stroke();\n`;
 }
 
+const pt = ([x, y]: XY) => `${`${x}`.padStart(3, ' ')},${`${y}`.padStart(3, ' ')}`;
+
 function genLineAsArray(line: SingleCurve) {
     const [p1, p2, c1, c2] = line.points;
-    const more = c2 ? ` [${c2[0]},${c2[1]}]` : '';
-    return `{points: [ [${p1[0]},${p1[1]}], [${p2[0]},${p2[1]}], [${c1[0]},${c1[1]}],${more} ]}`;
+    const more = c2 ? ` [${pt(c2)}]` : '';
+    return `{points: [ [${pt(p1)}], [${pt(p2)}], [${pt(c1)}],${more} ]}`;
 }
-
-// function genLineAsArray(line: SingleCurve) {
-//     const [p1, p2, c1, c2] = line.points;
-//     const path = c2
-//         ? `cp1: {x: ${c1[0]}, y: ${c1[1]}}, cp2: {x: ${c2[0]}, y: ${c2[1]}}`
-//         : `cp1: {x: ${c1[0]}, y: ${c1[1]}}`;
-//     return `{ p1: {x: ${p1[0]}, y: ${p1[1]}}, p2: {x: ${p2[0]}, y: ${p2[1]}}, ${path}  }`;
-// }
 
 function genAll(lines: SingleCurve[]) {
     return JSON.stringify(lines);
