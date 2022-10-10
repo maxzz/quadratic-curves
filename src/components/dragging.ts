bbf70ec69392e56df92901b586bb9847e135c413
Max Zakharzhevskiy <maxzz@msn.com>
2022-10-10 02:04:46 -0700

    WIP. dragging. code clean up

--- src/components/dragging.ts
+++ src/components/dragging.ts
@@ -4,8 +4,8 @@ import { SingleCurve } from "./types";
 
 type DraggingLine = {
     pt?: XY;
-    line?: SingleCurve;
-    idx: number;
+    curve?: SingleCurve;
+    curvePtIdx: number;
 };
 
 function getDragHandlersContext(appContext: AppContext, updateApp: (appContext: AppContext) => void) {
@@ -15,19 +15,17 @@ function getDragHandlersContext(appContext: AppContext, updateApp: (appContext: 
         //appContext.canvas.setPointerCapture(); //TODO: https://developer.mozilla.org/en-US/docs/Web/API/Element/setPointerCapture
 
         context = [];
-        let pt = mousePos(event);
+        const pt = mousePos(event);
 
-        const currentScene = appContext.scenes[appContext.current] || [];
+        const scene = appContext.scenes[appContext.current] || [];
 
-        // find the nearest point
-        for (var i = 0; i < currentScene.length; i++) {
-            var line: SingleCurve = currentScene[i];
+        // Find the nearest point
+        for (let i = 0; i < scene.length; i++) {
+            const curve: SingleCurve = scene[i];
 
-            //line.color === 'hsla(240, 100%, 50%, 0.95)' && console.log('----------', line);
-
-            let res = curveHasPoint(line, pt);
+            let res = curveHasPoint(curve, pt);
             if (res) {
-                context.push({ line: res.line, idx: res.idx, pt: pt, });
+                context.push({ curve: res.line, curvePtIdx: res.idx, pt: pt, }); // So far, it's just one point per curve
                 if (!appContext.checkDragGroup.checked) {
                     break;
                 }
@@ -45,10 +43,10 @@ function getDragHandlersContext(appContext: AppContext, updateApp: (appContext: 
         if (context.length) {
             let pos = mousePos(event);
             context.forEach((draggingLine: DraggingLine) => {
-                const line = draggingLine.line?.points[draggingLine.idx];
-                if (line && draggingLine.pt) {
-                    line[0] += pos[0] - draggingLine.pt[0];
-                    line[1] += pos[1] - draggingLine.pt[1];
+                const point = draggingLine.curve?.points[draggingLine.curvePtIdx];
+                if (point && draggingLine.pt) {
+                    point[0] += pos[0] - draggingLine.pt[0];
+                    point[1] += pos[1] - draggingLine.pt[1];
                     draggingLine.pt = pos;
                 }
             });
